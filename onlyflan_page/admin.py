8d8d49cdcaa2bcfc63bb65058aaeb5ac6210216a
eahumaded <eahumada@lag.cl>
2024-05-29 13:32:48 -0400

    Fix

--- onlyflan_page/admin.py
+++ onlyflan_page/admin.py
@@ -1,4 +1,3 @@
-# main/admin.py
 from django.contrib import admin
 from .models import Producto, Usuario
 
--- onlyflan_page/models.py
+++ onlyflan_page/models.py
@@ -13,8 +13,21 @@ class Producto(models.Model):
 
 class Usuario(AbstractUser):
     rut = models.CharField(max_length=12, unique=True)
-    nivel = models.IntegerField(default=0)
+    nivel = models.IntegerField(default=0)  # 0: usuario común, 10: administrador
     activado = models.BooleanField(default=True)
-
+    groups = models.ManyToManyField(
+            'auth.Group',
+            related_name='main_usuario_set',
+            blank=True,
+            help_text='The groups this user belongs to.',
+            verbose_name='groups'
+        )
+    user_permissions = models.ManyToManyField(
+            'auth.Permission',
+            related_name='main_usuario_set',
+            blank=True,
+            help_text='Specific permissions for this user.',
+            verbose_name='user permissions'
+        )
     def __str__(self):
         return self.username
